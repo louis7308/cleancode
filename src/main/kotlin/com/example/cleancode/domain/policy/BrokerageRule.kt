3a3d70f143f5e13ee7d17415f24c7aaaa5b4f5a6
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-21 17:40:32 +0900

    :sparkles: :: 중개 매매 수수료 한도 규정 class

---
+++ src/main/kotlin/com/example/cleancode/domain/policy/BrokerageRule.kt
@@ -0,0 +1,19 @@
+package com.example.cleancode.domain.policy
+
+import kotlin.math.floor
+
+// 가격이 특정 범위일 때 상한효율과 상한금액 가지는 클래스
+class BrokerageRule(
+    private val brokeragePercent: Double, // 상한요율
+    private val limitAmount: Long // 한도액
+) {
+    fun calcMaxBrokerage(price: Long): Long {
+        if (limitAmount == null) {
+            return multiplyPercent(price)
+        return Math.min(multiplyPercent(price), limitAmount)
+    }
+
+    private fun multiplyPercent(price: Long): Long {
+        return floor(brokeragePercent / 100 * price).toLong() // 소수점 자르기
+    }
+}
