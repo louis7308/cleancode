fdf356e24bea8888f0fba78e70cf6ee170f6c6dd
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-23 19:45:52 +0900

    :ambulance: :: Math.min으로 검사하던거는 Kotlin에서는 coerceAtMose 최소값 비교 변경

--- src/main/kotlin/com/example/cleancode/domain/policy/BrokerageRule.kt
+++ src/main/kotlin/com/example/cleancode/domain/policy/BrokerageRule.kt
@@ -5,12 +5,13 @@ import kotlin.math.floor
 // 가격이 특정 범위일 때 상한효율과 상한금액 가지는 클래스
 class BrokerageRule(
     private val brokeragePercent: Double, // 상한요율
-    private val limitAmount: Long // 한도액
+    private val limitAmount: Long? // 한도액
 ) {
     fun calcMaxBrokerage(price: Long): Long {
         if (limitAmount == null) {
             return multiplyPercent(price)
-        return Math.min(multiplyPercent(price), limitAmount)
+        }
+        return multiplyPercent(price).coerceAtMost(limitAmount)
     }
 
     private fun multiplyPercent(price: Long): Long {
